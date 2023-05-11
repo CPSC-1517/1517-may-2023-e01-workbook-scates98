3133af7bc0013f9dda5422292a26d205d645c707
scates98 <49077836+scates98@users.noreply.github.com>
2023-05-10 18:40:19 -0600

    Created Loop Example

--- GitHubPracticeSolution/GitHubPractice/Program.cs
+++ GitHubPracticeSolution/GitHubPractice/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Chris, This is the wonderful world of GitHub!");
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"iteration number {i}");
+}
