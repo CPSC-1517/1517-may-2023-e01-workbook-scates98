38dd52054ee40b1dc5a7b03e55c305e3f66be88f
Chris Cates <49077836+scates98@users.noreply.github.com>
2023-05-24 22:56:38 -0600

    Introduction to the Record class and added console application

---
+++ OOPs-Solution/SandBox/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ensure this CONSOLE APPLICATION knows about class by:
+// 1. Select project name (Sandbox) and expand to show Dependencies
+// 2. Right-click Dependencies and select Add Project Reference
+// 3. Check the appropriate project class library (OOPsReview)
+
+using OOPsReview;
+
+Console.WriteLine("Hello, World!");
+
+Residence myHome = new Residence(123, "Main St.", "Edmonton", "AB", "T6W3C8");
+// creates an instance of a READ-ONLY Record class named 'Residence'
+Console.WriteLine(myHome.ToString());
+// Can a value be changed in a record instance? NO!
+// myHome.Number = 321; // <-- This does NOT work (commented out)
+// To alter a value in a record instance, you must create a new instance
+
+myHome = new Residence(1051, "Main St.", "Edmonton", "AB", "T6W3C8");
+Console.WriteLine(myHome.ToString());
