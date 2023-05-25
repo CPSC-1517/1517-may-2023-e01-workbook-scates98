f3b9784481cc5ace734bf11c666ab918150d5037
Chris Cates <49077836+scates98@users.noreply.github.com>
2023-05-24 22:11:14 -0600

    Created public record  class 'Residence'

---
+++ OOPs-Solution/OOPsReview/Residence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public record Residence (int Number, string Street, string City, string Province, string PostalCode)
+        // Creates a READ-ONLY Class named 'Residence'
+        // Can initialize values when created, but cannot alter values after created
+    {
+        // Method
+        public override string ToString()
+        {
+            return $"{Number},{Street},{City},{Province},{PostalCode}";
+        }
+    }
+}
