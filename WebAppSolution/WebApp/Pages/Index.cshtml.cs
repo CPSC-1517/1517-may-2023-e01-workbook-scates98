71c1059713fde3dd571c4ad00ed732c1806505b9
Chris Cates <49077836+scates98@users.noreply.github.com>
2023-06-12 22:23:20 -0600

    AssessorReview

--- WebAppSolution/WebApp/Pages/Index.cshtml.cs
+++ WebAppSolution/WebApp/Pages/Index.cshtml.cs
@@ -4,14 +4,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
+        // : PageModel is the INHERITANCE!
     {
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
+            // a constructor can bring in a variable (logger)
+            // the parameter has a data type
         {
             _logger = logger;
+            // can assign the value of the paramter to the data type
         }
 
+        // METHOD 
         public void OnGet()
         {
 
