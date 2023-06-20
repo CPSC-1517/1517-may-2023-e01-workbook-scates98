990be0d9001cc65bff350a7993efe6fc1b30c541
Chris Cates <49077836+scates98@users.noreply.github.com>
2023-06-19 18:50:26 -0600

    Added dropdown list (select with options) and printed selected/entered data

--- WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -6,12 +6,46 @@ namespace WebApp.Pages.Samples
     public class BasicDataManagementModel : PageModel
     {
         // form control properties:
+        // it is the asp-for that links the form control to the property
+        // PROBLEM:
+        // if a property does NOT have a [BindProperty] attribute,
+        //      the value of the property is NOT updated when the form is submitted
+        //      the property is considered as a read-only property (one-way binding)
+        //      the property value is "out-going only" (from the server to the client)
+
+        //[BindProperty]    // this attribute is not required 
+        // However, if you wish to receive data from your client (form)
+        //      into the server (PageModel) you MUST use have the
+        //      [BindProperty] attribute on the property
+        // The property will be  a two-way binding
+        //  The property value is BOTH "out-going" and "in-coming"
+
+        [BindProperty]
         public int Num { get; set; }
 
-        public string MassText { get; set; }  
+        [BindProperty]
+        public string MassText { get; set; }
+
+        [BindProperty]
+        public int FavouriteCourse { get; set; } // using intege from select 
+
+        [BindProperty]
+        public string FavouriteCourseNoValueOnOption { get; set; } // using default string from select
+
+        [BindProperty]
+        public string Feedback { get; set; }    // one-way property for messages
 
         public void OnGet()
         {
+        }
+
+        public void OnPostControlProcessing()
+        {
+            Feedback = "You pressed the button!"
+                + $" Number value is: {Num}"
+                + $"  Mass text is: {MassText}"
+                + $"  Favourite course with value is: {FavouriteCourse}"
+                + $"  Favourite course with value is: {FavouriteCourseNoValueOnOption}";
         }
     }
 }
