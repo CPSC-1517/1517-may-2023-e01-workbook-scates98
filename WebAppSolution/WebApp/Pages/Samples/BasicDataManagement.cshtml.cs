8a7d3d36ad33292cc035e08462f21ac7059838ba
Chris Cates <49077836+scates98@users.noreply.github.com>
2023-06-23 18:51:36 -0600

    Added button to link to another page (BasicDataManagement.chtmls)

--- WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -24,7 +24,8 @@ namespace WebApp.Pages.Samples
         public int Num { get; set; }
 
         [BindProperty]
-        public string MassText { get; set; }
+        public string? MassText { get; set; }
+        // IMPORTANT - QUESTION MARK ? indicates that this property is nullable (not required/optional)
 
         [BindProperty]
         public int FavouriteCourse { get; set; } // using intege from select 
@@ -33,7 +34,7 @@ namespace WebApp.Pages.Samples
         public string FavouriteCourseNoValueOnOption { get; set; } // using default string from select
 
         [BindProperty]
-        public string Feedback { get; set; }    // one-way property for messages
+        public string? Feedback { get; set; }    // one-way property for messages
 
 
         // This is an example of you managing the collection of errors on the form yourself
@@ -45,13 +46,123 @@ namespace WebApp.Pages.Samples
         {
         }
 
-        public void OnPostControlProcessing()
+        // NOTE: The return datatype of 'void' stays on the same page
+        //          Whereas IActionResult returns      
+        //public void OnPostControlProcessing()
+        //{
+        //    //Feedback = "You pressed the button!"
+        //    //    + $" Number value is: {Num}"
+        //    //    + $"  Mass text is: {MassText}"
+        //    //    + $"  Favourite course with value is: {FavouriteCourse}"
+        //    //    + $"  Favourite course with value is: {FavouriteCourseNoValueOnOption}";
+
+        //    if(Num<0)
+        //    {
+        //        // Using ModelState
+        //        ModelState.AddModelError("", $"Num value of {Num} cannot be negative");
+
+        //        // Managing your own errors 
+        //        ErrorList.Add($"Num value of {Num} cannot be negative");
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(MassText))
+        //    {
+        //        // Using ModelState
+        //        ModelState.AddModelError("", $"MassText value cannot be empty");
+
+        //        // Managing your own errors 
+        //        ErrorList.Add($"MassText value cannot be empty");
+        //    }
+
+        //    if (FavouriteCourse == 0)
+        //    {
+        //        // Using ModelState
+        //        ModelState.AddModelError("", $"FavouriteCourse value of {FavouriteCourse} cannot be null");
+
+        //        // Managing your own errors 
+        //        ErrorList.Add($"FavouriteCourse value of {FavouriteCourse} cannot be null");
+        //    }
+
+        //    if (FavouriteCourseNoValueOnOption == "On Screen prompt line ...")
+        //    {
+        //        // Using ModelState
+        //        ModelState.AddModelError("", $"FavouriteCourseNoValueOnOption value cannot be null");
+
+        //        // Managing your own errors 
+        //        ErrorList.Add($"FavouriteCourseNoValueOnOption value cannot be null");
+        //    }
+
+        //    // if(ErrorList.Count()==0)
+        //    if (ModelState.IsValid)      // ModelState checks for you inside the AddModelError method
+        //    {
+        //        Feedback = "Your data was valid.";
+        //    }
+
+        //}
+        
+        public IActionResult OnPostControlProcessing()
         {
-            Feedback = "You pressed the button!"
-                + $" Number value is: {Num}"
-                + $"  Mass text is: {MassText}"
-                + $"  Favourite course with value is: {FavouriteCourse}"
-                + $"  Favourite course with value is: {FavouriteCourseNoValueOnOption}";
+            //Feedback = "You pressed the button!"
+            //    + $" Number value is: {Num}"
+            //    + $"  Mass text is: {MassText}"
+            //    + $"  Favourite course with value is: {FavouriteCourse}"
+            //    + $"  Favourite course with value is: {FavouriteCourseNoValueOnOption}";
+
+            if (Num < 0)
+            {
+                // Using ModelState
+                ModelState.AddModelError("", $"Num value of {Num} cannot be negative");
+
+                // Managing your own errors 
+                ErrorList.Add($"Num value of {Num} cannot be negative");
+            }
+
+            if (string.IsNullOrWhiteSpace(MassText))
+            {
+                // Using ModelState
+                ModelState.AddModelError("", $"MassText value cannot be empty");
+
+                // Managing your own errors 
+                ErrorList.Add($"MassText value cannot be empty");
+            }
+
+            if (FavouriteCourse == 0)
+            {
+                // Using ModelState
+                ModelState.AddModelError("", $"FavouriteCourse value of {FavouriteCourse} cannot be null");
+
+                // Managing your own errors 
+                ErrorList.Add($"FavouriteCourse value of {FavouriteCourse} cannot be null");
+            }
+
+            if (FavouriteCourseNoValueOnOption == "On Screen prompt line ...")
+            {
+                // Using ModelState
+                ModelState.AddModelError("", $"FavouriteCourseNoValueOnOption value cannot be null");
+
+                // Managing your own errors 
+                ErrorList.Add($"FavouriteCourseNoValueOnOption value cannot be null");
+            }
+
+            // if(ErrorList.Count()==0)
+            if (ModelState.IsValid)      // ModelState checks for you inside the AddModelError method
+            {
+                Feedback = "Your data was valid.";
+            }
+            return Page(); // This statement is required because we changed the return datatype from
+                           //   void to IActionResult
+                           // The action is to stay on the same page
+
         }
+        public IActionResult OnPostRedirectPage()
+        {
+            // IActionResult requires the event to have a return statement with some type of action
+            // In this example the action is to redirect to the page supplied as the supplied 
+            //      parameter value
+            // An alternative to using IActionResult to stay on the same page is to use Return Page;
+            return RedirectToPage("BasicEvents");
+        }
+
+       
     }
 }
